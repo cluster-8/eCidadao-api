92dc412b67d4985bc9a2306ad3078050f0b890e3
Hariel <hariel@hotmail.com>
2022-09-14 08:06:29 -0300

    Appmodule add utilsModule import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Module, RequestMethod } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ModulesModule } from './modules/modules.module';
 import { ProvidersModule } from './providers/providers.module';
+import { UtilsModule } from './utils/utils.module';
 
 @Module({
   imports: [
@@ -9,10 +10,26 @@ import { ProvidersModule } from './providers/providers.module';
       envFilePath: [`.env`],
       isGlobal: true,
     }),
+    UtilsModule,
     ModulesModule,
     ProvidersModule,
   ],
   providers: [ConfigService],
   exports: [ConfigService],
 })
-export class AppModule {}
+export class AppModule {
+  ensureAuthenticatedExclude = [
+    { path: '/v1/auth/sign-in', method: RequestMethod.POST },
+    { path: '/v1/user', method: RequestMethod.POST },
+  ];
+
+  // configure(consumer: MiddlewareConsumer) {
+  //   consumer
+  //     .apply(EnsureAuthenticated)
+  //     .exclude(...this.ensureAuthenticatedExclude)
+  //     .forRoutes({
+  //       path: '*',
+  //       method: RequestMethod.ALL,
+  //     });
+  // }
+}
