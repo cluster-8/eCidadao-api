0050a89d1b0fbc9577c5bfe9c7ea6cf3549201d9
Hariel <hariel@hotmail.com>
2022-09-14 08:07:19 -0300

    AuthModule

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignInDto } from './dto/sign.in.dto';
+
+@Controller({ path: 'auth', version: '1' })
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/sign-in')
+  async signIn(@Body() body: SignInDto) {
+    return this.authService.signIn(body);
+  }
+}
---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,37 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '@src/providers/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { SignInDto } from './dto/sign.in.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly prisma: PrismaService, private readonly jwtService: JwtService) {
+    setTimeout(async () => {
+      console.log('admin', await bcrypt.hashSync('admin', 10));
+      console.log('technical', await bcrypt.hashSync('technical', 10));
+    }, 1500);
+  }
+
+  generateToken(id: string, email: string, role: string) {
+    const payload = { id: id, email: email, role: role };
+
+    const token = this.jwtService.sign(payload);
+
+    return token;
+  }
+
+  async signIn(body: SignInDto) {
+    const user = await this.prisma.user.findFirst({ where: { email: body.email } });
+
+    if (!user) throw new BadRequestException('Incorrect email/password combination');
+
+    const passwordMatched = await bcrypt.compare(body.password, user.password);
+
+    if (!passwordMatched) throw new BadRequestException('Incorrect email/password combination');
+
+    const token = this.generateToken(user.id, user.email, user.role);
+
+    return { user, token };
+  }
+}
