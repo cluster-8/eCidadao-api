8ac6940d9986f3223056e7c8854d5e6dc084e424
Hariel <hariel@hotmail.com>
2022-09-23 09:12:43 -0300

    AuthModule add AuthService to exports

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -6,5 +6,6 @@ import { AuthController } from './auth.controller';
   imports: [],
   controllers: [AuthController],
   providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
