5beb1d6a2ee6e6934ce86c446ecfb51471f4ee22
Hariel <hariel@hotmail.com>
2022-10-27 17:57:53 -0300

    feat: EncrypterService

--- src/utils/utils.module.ts
+++ src/utils/utils.module.ts
@@ -1,11 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { GuardsModule } from './guards/guards.module';
+import { Encrypter } from './services/encrypter.service';
 import { Request } from './services/request.service';
 
 @Global()
 @Module({
   imports: [GuardsModule],
-  providers: [Request],
-  exports: [Request],
+  providers: [Request, Encrypter],
+  exports: [Request, Encrypter],
 })
 export class UtilsModule {}
