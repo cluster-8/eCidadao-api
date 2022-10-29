5d6663151a98fef2b2d12451fe77326caabfcd06
Hariel <hariel@hotmail.com>
2022-10-29 09:46:11 -0300

    refactor: app.service - remove encrypter test

--- src/modules/modules.module.ts
+++ src/modules/modules.module.ts
@@ -3,11 +3,10 @@ import { AuthModule } from './auth/auth.module';
 import { RequestsModule } from './requests/requests.module';
 import { UserModule } from './user/user.module';
 import { UsageTermsModule } from './usage-terms/usage-terms.module';
-import { AppService } from './app.service';
 
 @Module({
   imports: [AuthModule, RequestsModule, UserModule, UsageTermsModule],
-  providers: [AppService],
+  providers: [],
   exports: [],
 })
 export class ModulesModule {}
