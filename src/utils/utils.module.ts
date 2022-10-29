047113839674ce268bb6227013241e68c91940cb
Hariel <hariel@hotmail.com>
2022-10-29 09:44:15 -0300

    feat: remvoe encrypter.service - refactor to encrypter.fn

--- src/utils/utils.module.ts
+++ src/utils/utils.module.ts
@@ -1,12 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { GuardsModule } from './guards/guards.module';
-import { Encrypter } from './services/encrypter.service';
 import { Request } from './services/request.service';
 
 @Global()
 @Module({
   imports: [GuardsModule],
-  providers: [Request, Encrypter],
-  exports: [Request, Encrypter],
+  providers: [Request],
+  exports: [Request],
 })
 export class UtilsModule {}
